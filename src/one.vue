c21110c2e8f7866b59a5346f74fb5987776717f8
huangguiwen <amwsffyygy@163.com>
2018-06-11 19:00:55 +0800

    .

---
+++ src/one.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="one">
+    {{msg}}
+    <a href="index.html">index</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "one",
+  data() {
+    return {
+      msg: "I am one"
+    };
+  }
+};
+</script>
